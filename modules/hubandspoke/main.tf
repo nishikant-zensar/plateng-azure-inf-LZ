f1e6e4288becdfea6b2e6201b11804b6f7c1b6cf
nishikant-zensar <nishikant.sinha@zensar.com>
2025-07-23 11:37:54 +0530

    Update main.tf

--- modules/hubandspoke/main.tf
+++ modules/hubandspoke/main.tf
@@ -1,1 +1,9 @@
-
+provider "azurerm" {
+  features        = {}
+  subscription_id = var.subscription_id
+}
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
