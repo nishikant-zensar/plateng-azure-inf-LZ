ad267ceaf7ada85d8ad59507b5e606b0e162bf8f
nishikant-zensar <nishikant.sinha@zensar.com>
2025-07-31 12:39:56 +0530

    Updated values for subscriptions

--- modules/managementgroups/main.tf
+++ modules/managementgroups/main.tf
@@ -21,52 +21,144 @@ provider "azurerm" {
   features {}
 }
 
-
-resource "azurerm_management_group" "platform" {
-  name        = "platform"
-  display_name = "platform"
-  parent_management_group_id = var.root_management_group_id # Use the root management group ID directly
+####################################
+# Create the Top Management Group  #
+####################################
+resource "azurerm_management_group" "TescoIMSRootMG" {
+  name         = "IMS-Root"       # Unique name for the management group
+  display_name = "IMS-Root"       # Friendly display name
 }
 
-resource "azurerm_management_group" "environments" {
-  name        = "environments"
-  display_name = "environments"
-  parent_management_group_id = var.root_management_group_id # Use the root management group ID directly
+##############################
+# Create 1st Level Child MGs #
+##############################
+# Platform Root Management Group
+resource "azurerm_management_group" "ims-root-platform" {
+  name                          = "ims-root-platform"
+  display_name                  = "ims-root-platform"
+  parent_management_group_id    = azurerm_management_group.TescoIMSRootMG.id
+  depends_on = [
+    azurerm_management_group.TescoIMSRootMG
+  ]
+}
+# Environments Root Management Group
+resource "azurerm_management_group" "ims-root-environments" {
+  name                          = "ims-root-environments"
+  display_name                  = "ims-root-environments"
+  parent_management_group_id    = azurerm_management_group.TescoIMSRootMG.id
+  depends_on = [
+    azurerm_management_group.TescoIMSRootMG
+  ]
+}
+# Sandbox Root Management Group
+resource "azurerm_management_group" "ims-root-sandbox" {
+  name                          = "ims-root-sandbox"
+  display_name                  = "ims-root-sandbox"
+  parent_management_group_id    = azurerm_management_group.TescoIMSRootMG.id
+  depends_on = [
+    azurerm_management_group.TescoIMSRootMG
+  ]
+}
+# Decommission Root Management Group
+resource "azurerm_management_group" "ims-root-decommission" {
+  name                          = "ims-root-decommission"
+  display_name                  = "ims-root-decommission"
+  parent_management_group_id    = azurerm_management_group.TescoIMSRootMG.id
+  depends_on = [
+    azurerm_management_group.TescoIMSRootMG
+  ]
 }
 
-resource "azurerm_management_group" "Development" {
-  name        = "ims-environments-development"
-  display_name = "ims-environments-development"
-  parent_management_group_id = azurerm_management_group.environments.id
+#################################################
+# Create Child MGs under "ims-root-platform" MG #
+################################################
+# 1. prd platform MG under "ims-root-platform" MG
+resource "azurerm_management_group" "ims-platform-prd" {
+  name                          = "ims-platform-prd"
+  display_name                  = "ims-platform-prd"
+  parent_management_group_id    = azurerm_management_group.ims-root-platform.id
+  depends_on = [
+    azurerm_management_group.ims-root-platform
+  ]
 }
 
-resource "azurerm_management_group" "envproduction" {
-  name        = "ims-environments-production"
-  display_name = "ims-environments-production"
-  parent_management_group_id = azurerm_management_group.environments.id
+# 2. ppte platform MG under "ims-root-platform" MG
+resource "azurerm_management_group" "ims-platform-ppte" {
+  name                          = "ims-platform-ppte"
+  display_name                  = "ims-platform-ppte"
+  parent_management_group_id    = azurerm_management_group.ims-root-platform.id
+  depends_on = [
+    azurerm_management_group.ims-root-platform
+  ]
 }
 
-resource "azurerm_management_group" "sandbox" {
-  name        = "sandbox"
-  display_name = "sandbox"
-  parent_management_group_id = var.root_management_group_id # Use the root management group ID directly
+#####################################################
+# Create Child MGs under "ims-root-environments" MG #
+#####################################################
+# 1.  dev MG under "ims-root-environments" MG
+resource "azurerm_management_group" "ims-env-dev" {
+  name                          = "ims-env-dev"
+  display_name                  = "ims-env-dev"
+  parent_management_group_id    = azurerm_management_group.ims-root-environments.id
+  depends_on = [
+    azurerm_management_group.ims-root-environments
+  ]
 }
 
-resource "azurerm_management_group" "decommissioned" {
-  name        = "decommissioned"
-  display_name = "decommissioned"
-  parent_management_group_id = var.root_management_group_id # Use the root management group ID directly
+# 2. ppe (pre-production) MG under "ims-root-environments" MG
+resource "azurerm_management_group" "ims-env-ppe" {
+  name                          = "ims-env-ppe"
+  display_name                  = "ims-env-ppe"
+  parent_management_group_id    = azurerm_management_group.ims-root-environments.id
+  depends_on = [
+    azurerm_management_group.ims-root-environments
+  ]
+}
+# 3. test MG under "ims-root-environments" MG
+resource "azurerm_management_group" "ims-env-test" {
+  name                          = "ims-env-test"
+  display_name                  = "ims-env-test"
+  parent_management_group_id    = azurerm_management_group.ims-root-environments.id
+  depends_on = [
+    azurerm_management_group.ims-root-environments
+  ]
+}
+# 4. prd MG under "ims-root-environments" MG
+resource "azurerm_management_group" "ims-env-prd" {
+  name                          = "ims-env-prd"
+  display_name                  = "ims-env-prd"
+  parent_management_group_id    = azurerm_management_group.ims-root-environments.id
+  depends_on = [
+    azurerm_management_group.ims-root-environments
+  ]
 }
 
-resource "azurerm_management_group" "non_prod" {
-  name        = "ims-platform-nonprod"
-  display_name = "ims-platform-nonprod"
-  parent_management_group_id = azurerm_management_group.platform.id
+##############################################
+# Associate subscriptions to respective MG's #
+##############################################
+# 1. Associate the Connectivity subscription with "ims-platform-prd" MG
+resource "azurerm_management_group_subscription_association" "ims-platform-prd-Connectivity" {
+  management_group_id = azurerm_management_group.ims-platform-prd.id
+  subscription_id     = "/subscriptions/eca8a48a-2dc4-45da-908c-94bf6100016c"
+  depends_on = [
+    azurerm_management_group.ims-platform-prd
+  ]
 }
 
+# 2. Associate the Management subscription with "ims-platform-prd" MG
+resource "azurerm_management_group_subscription_association" "ims-platform-prd-management" {
+  management_group_id = azurerm_management_group.ims-platform-prd.id
+  subscription_id     = "/subscriptions/b63f4e55-499d-4984-9375-f17853ff6e36"
+  depends_on = [
+    azurerm_management_group.ims-platform-prd
+  ]
+}
 
-resource "azurerm_management_group" "production" {
-  name        = "ims-platform-prod"
-  display_name = "ims-platform-production"
-  parent_management_group_id = azurerm_management_group.platform.id
+# 3. Associate AVD Production subscription with "ims-env-prd" MG
+resource "azurerm_management_group_subscription_association" "ims-env-prd-avd" {
+  management_group_id = azurerm_management_group.ims-env-prd.id
+  subscription_id     = "/subscriptions/9da3ee14-3ae9-4be0-9ad2-b9a7c7b059ef"
+  depends_on = [
+    azurerm_management_group.ims-env-prd
+  ]
 }
