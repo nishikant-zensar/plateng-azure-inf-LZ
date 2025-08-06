72a83ecde47f32ebd17db8ce707dfce639d7e690
nishikant-zensar <nishikant.sinha@zensar.com>
2025-08-06 19:25:49 +0530

    Updated variables

--- modules/hubandspoke/variables.tf
+++ modules/hubandspoke/variables.tf
@@ -22,36 +22,11 @@ variable "backend_container_name" {
 variable "backend_key" {
   description = "The key for the backend state file"
   type        = string
-  default     = "managementgroups.terraform.tfstate"
+  default     = "hubspoke.terraform.tfstate"
 }
-# variable "tags" {
- # description = "Tags to assign to the Azure resources"
- # type        = map(string)
- # default     = {
- #   Name        = "ims-prd-avd-ne-rg-network"
- #   Environment = "prd"
- #   DateCreated = "2025-07-01"
- # }
-# }
 variable "subscription_id" {
   description = "The subscription ID to deploy the resource group"
   type        = string
-}
-
-variable "sub1_subscription_id" {
-  description = "Subscription ID for sub1"
-  type        = string
-}
-
-variable "sub2_subscription_id" {
-  description = "Subscription ID for sub2"
-  type        = string
-}
-
-variable "tags" {
-  description = "Tags to assign to the Azure resource"
-  type        = map(string)
-  default     = {}
 }
 variable "location" {
   description = "Location for resource creation"
