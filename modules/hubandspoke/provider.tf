85c9efa9cbd3b4fd5447aaa7a0c6f9ce170ffa90
nishikant-zensar <nishikant.sinha@zensar.com>
2025-08-06 19:16:53 +0530

    Updated provider

--- modules/hubandspoke/provider.tf
+++ modules/hubandspoke/provider.tf
@@ -1,14 +1,4 @@
 provider "azurerm" {
-  # Configuration options
-  subscription_id = "eca8a48a-2dc4-45da-908c-94bf6100016c"
-  tenant_id = "684d2402-0ea6-442d-9ad7-4ef26b925ec5"
-  client_id = "74925104-cd8b-47e5-b29a-83a75a2f4ca6" # Application (Client ID)
-  # client_secret = "XXXXX" # Value of the client ID
-  features {}
- 
-}
-
-provider "azurerm" {
   alias           = "ims-prd-connectivity"
   subscription_id = "eca8a48a-2dc4-45da-908c-94bf6100016c"
   tenant_id       = "684d2402-0ea6-442d-9ad7-4ef26b925ec5"
