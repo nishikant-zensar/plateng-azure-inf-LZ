9e26b2511131d4f925127e0110d55fd3acfc6e67
nishikant-zensar <nishikant.sinha@zensar.com>
2025-08-06 20:28:18 +0530

    Updates

--- modules/hubandspoke/provider.tf
+++ modules/hubandspoke/provider.tf
@@ -19,8 +19,6 @@ provider "azurerm" {
   subscription_id = "9da3ee14-3ae9-4be0-9ad2-b9a7c7b059ef"
   tenant_id       = "684d2402-0ea6-442d-9ad7-4ef26b925ec5"
   client_id       = "74925104-cd8b-47e5-b29a-83a75a2f4ca6"
+  resource_provider_registrations = "none"
   features {}
 }
-provider "azurerm" {
-  resource_provider_registrations = "none"
- }
