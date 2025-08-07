389672e87f6f1cf0fa858e7bc162abb3c809848b
nishikant-zensar <nishikant.sinha@zensar.com>
2025-08-07 19:40:43 +0530

    Updated provider

--- modules/networkrules/main.tf
+++ modules/networkrules/main.tf
@@ -256,6 +256,15 @@ terraform {
 ###################
 # Create UDR 
 ###################
+
+provider "azurerm" {
+  alias           = "ims-prd-connectivity"
+  subscription_id = "ecd60543-12a0-4899-9e5f-21ec01592207"
+  tenant_id       = "684d2402-0ea6-442d-9ad7-4ef26b925ec5"
+  client_id       = "74925104-cd8b-47e5-b29a-83a75a2f4ca6"
+  features {}
+}
+
 #1. Create a udr to associate with "GatewaySubnet" subnet in hub vNet
 resource "azurerm_route_table" "ims-prd-conn-ne-rt-vpng" {
   provider            = azurerm.ims-prd-connectivity
