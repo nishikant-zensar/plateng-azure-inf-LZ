9f45c3479ff6c2738fed02049002c2085e3784d0
nishikant-zensar <nishikant.sinha@zensar.com>
2025-08-12 18:47:43 +0530

    Update main.tf

--- modules/azureservices/main.tf
+++ modules/azureservices/main.tf
@@ -119,12 +119,38 @@ resource "azurerm_firewall_policy_rule_collection_group" "coreplat_group" {
       destination_ports     = ["80", "443"]
       description           = "Probably best creating an IP Group with these zscaler IPs, rather than adding them to this rule individually, as it's easier to manage if the IPs change in future."
     }
+    
+rule {
+      name                  = "ims-prd-conn-ne-afwpr-mgmtst-out"
+      source_addresses      = ["192.168.10.0/24"]
+      destination_service_tags = ["WindowsVirtualDesktop, AzureMonitor, EventHub"]
+      protocols             = ["TCP"]
+      destination_ports     = ["443"]
+      description           = "The AVD session hosts needs to access this list of FQDNs and endpoints for Azure Virtual Desktop. All entries are outbound, it is not required to open inbound ports for AVD"
+    }
+rule {
+      name                  = "ims-prd-conn-ne-afwpr-mgmtip-out"
+      source_addresses      = ["192.168.10.0/24"]
+      destination_addresses = ["169.254.169.254,168.63.129.16"]
+      protocols             = ["TCP"]
+      destination_ports     = ["80"]
+      description           = "The AVD session hosts needs to access this list of FQDNs and endpoints for Azure Virtual Desktop. All entries are outbound, it is not required to open inbound ports for AVD."
+    }
   }
-  # application_rule_collection {
-  #  name     = "ims-prd-conn-ne-afwprc-coreplat-app"
-  #  priority = 300
-  #  action   = "Allow"
-  # }
+   application_rule_collection {
+    name     = "ims-prd-conn-ne-afwprc-coreplat-app"
+    priority = 300
+    action   = "Allow"
+
+  rule {
+      name                  = "ims-prd-conn-ne-afwpr-mgmtfqdn-out"
+      source_addresses      = ["192.168.10.0/24"]
+      destination_fqdns     = ["login.microsoftonline.com,*.wvd.microsoft.com,catalogartifact.azureedge.net,*.prod.warm.ingest.monitor.core.windows.net,gcs.prod.monitoring.core.windows.net,azkms.core.windows.net,mrsglobalsteus2prod.blob.core.windows.net,wvdportalstorageblob.blob.core.windows.net,oneocsp.microsoft.com,www.microsoft.com,aka.ms,login.windows.net,*.events.data.microsoft.com,www.msftconnecttest.com,*.prod.do.dsp.mp.microsoft.com,*.sfx.ms,*.digicert.com,*.azure-dns.com,*.azure-dns.net,*eh.servicebus.windows.net"]
+      protocols             = ["http","https"]
+      destination_ports     = ["80","443","1688"]
+      description           = "The AVD session hosts needs to access these FQDNs and endpoints for Azure Virtual Desktop. All entries are outbound, it is not required to open inbound ports for AVD."
+    }
+   }
 }
 
 #####################################################################
@@ -363,6 +389,7 @@ resource "azurerm_private_dns_zone" "multi" {
   name                = each.value
   resource_group_name = data.azurerm_resource_group.connsub.name
 }
+
 
 
 
