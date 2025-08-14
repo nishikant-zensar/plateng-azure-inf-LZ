9c1b13873fb2825579eb17918c06fe58844339e8
nishikant-zensar <nishikant.sinha@zensar.com>
2025-08-14 13:15:06 +0530

    Update main.tf

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -229,3 +229,51 @@ resource "azurerm_local_network_gateway" "aws_lgw4" {
 #    DateCreated   = "2025-08-01"
 #}
 # }
+
+# 8. Create Gateway Connection 3 on VPN Gateway
+
+#resource "azurerm_virtual_network_gateway_connection" "s2s_connection3" {
+#  name                            = "ims-prd-conn-ne-vnc-03"
+#  location                        = var.location
+#  resource_group_name             = var.vnet_resource_group
+#  type                            = "IPsec"
+#  virtual_network_gateway_id      = "/subscriptions/ecd60543-12a0-4899-9e5f-21ec01592207/resourceGroups/ims-prd-conn-ne-rg-network/providers/Microsoft.Network/virtualNetworkGateways/ims-prd-conn-ne-vpng-01"
+#  local_network_gateway_id        = "/subscriptions/ecd60543-12a0-4899-9e5f-21ec01592207/resourceGroups/ims-prd-conn-ne-rg-network/providers/Microsoft.Network/localNetworkGateways/ims-prd-conn-ne-lgw-aws-02"
+#  connection_protocol             = "IKEv2"
+#  shared_key                      = "gyRTAP4mgsUbmTcTqJBQCU02ChqzRvSX" # Replace with your actual pre-shared key
+#  dpd_timeout_seconds             = 45
+#  use_policy_based_traffic_selectors = true
+
+  # IPsec/IKE policy is default (no custom policy block)
+  # NAT Rules not configured
+
+#  tags = {
+#    Name          = "ims-prd-conn-ne-vnc-02"
+#    Environment   = "prd"
+#    DateCreated   = "2025-08-01"
+#}
+# }
+
+# 9. Create Gateway Connection 4 on VPN Gateway
+
+#resource "azurerm_virtual_network_gateway_connection" "s2s_connection4" {
+#  name                            = "ims-prd-conn-ne-vnc-04"
+#  location                        = var.location
+#  resource_group_name             = var.vnet_resource_group
+#  type                            = "IPsec"
+#  virtual_network_gateway_id      = "/subscriptions/ecd60543-12a0-4899-9e5f-21ec01592207/resourceGroups/ims-prd-conn-ne-rg-network/providers/Microsoft.Network/virtualNetworkGateways/ims-prd-conn-ne-vpng-01"
+#  local_network_gateway_id        = "/subscriptions/ecd60543-12a0-4899-9e5f-21ec01592207/resourceGroups/ims-prd-conn-ne-rg-network/providers/Microsoft.Network/localNetworkGateways/ims-prd-conn-ne-lgw-aws-02"
+#  connection_protocol             = "IKEv2"
+#  shared_key                      = "gyRTAP4mgsUbmTcTqJBQCU02ChqzRvSX" # Replace with your actual pre-shared key
+#  dpd_timeout_seconds             = 45
+#  use_policy_based_traffic_selectors = true
+
+  # IPsec/IKE policy is default (no custom policy block)
+  # NAT Rules not configured
+
+#  tags = {
+#    Name          = "ims-prd-conn-ne-vnc-03"
+#    Environment   = "prd"
+#    DateCreated   = "2025-08-01"
+#}
+# }
